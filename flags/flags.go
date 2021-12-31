bde2ec0034139220422dbe714d1aedcf9892a005
Dennis Cope <dennisaaroncope@gmail.com>
2021-12-31 02:23:19 -0500

    Partial flags setup

--- flags/flags.go
+++ flags/flags.go
@@ -2,8 +2,19 @@ package flags
 
 import "flag"
 
+type Flags interface {
+	sesUsername 
+	sesPassword
+}
+
 var sesUsername = flag.String("username", "", "Username credential")
 var sesPassword = flag.String("password", "", "Password credential")
+var production = flag.Bool("production", false, "Production mode")
+
 // Set up flags
 flag.Parse()
+
+func Flags() Flags {
+
+}
 
